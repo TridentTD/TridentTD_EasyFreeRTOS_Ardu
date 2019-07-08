8d7a9b72e416fff447ebab58de8696a02b91400f
TridentTD <prote2000@gmail.com>
2019-07-08 12:32:52 +0700

    Update TridentTD_EasyFreeRTOS_Ardu.h

--- src/TridentTD_EasyFreeRTOS_Ardu.h
+++ src/TridentTD_EasyFreeRTOS_Ardu.h
@@ -2,9 +2,9 @@
 #define __TRIDENTTD_EASY_FREERTOS_ARDU_H__
 
 #include <Arduino.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/semphr.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
 
 #ifndef TridentOS 
 #define TridentOS  EasyFreeRTOS_Ardu
